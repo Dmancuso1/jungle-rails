daf410ac3bcebf73307d60658ae87e2e18770f33
Dane Mancuso <danegmancuso@gmail.com>
2020-08-20 19:02:13 +0000

    login feature works to prevent visitor from seeing an authorized page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,13 @@ class UsersController < ApplicationController
       redirect_to '/signup'
   end
 
+
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
+  # ************* USER AUTH *****************
   # This route sends requests to our naked url to the *cool* action in the *gif* controller.
   # root to: 'gif#cool'
 
@@ -30,10 +31,20 @@ Rails.application.routes.draw do
   # get '/cool' => 'gif#cool'
   # get '/sweet' => 'gif#sweet'
 
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+
+
+  #*******************************************
 
 
   # The priority is based upon order of creation: first created -> highest priority.
